b07bbb4b17fe50c5f28acd5d044f6d820d287369
mariuswallin <post@mariuswallin.no>
2023-09-30 11:33:37 +0200

    feat: add comments to webapp-03

--- webapp-03/src/app/responses/page.tsx
+++ webapp-03/src/app/responses/page.tsx
@@ -1,8 +1,10 @@
+// Importing React and necessary dependencies
 "use client"
 
 import { useEffect, useState } from "react"
 import type { FilterOption } from "@/hooks/useMultiFilter"
 
+// Importing custom components and types
 import { Filter } from "@/components/Filter"
 import ResponseItem from "@/components/ResponseItem"
 import Responses from "@/components/Responses"
@@ -10,9 +12,12 @@ import { TableHead } from "@/components/Table/TableHead"
 import { type Response } from "@/features/responses/types"
 import { useMultiFilter } from "@/hooks/useMultiFilter"
 
+// Defining the ResponsePage component
 export default function ResponsePage() {
+  // State to store the responses data
   const [responses, setResponses] = useState<Response[]>([])
 
+  // Filter options for filtering the responses
   const filterOptions: FilterOption<Response>[] = [
     {
       key: "score",
@@ -25,12 +30,15 @@ export default function ResponsePage() {
       values: (items) => items.map((item) => item.category),
     },
   ]
+
+  // Using the custom useMultiFilter hook to handle filtering
   const { data, handleFilter, filter, resetFilter, options } =
     useMultiFilter<Response>({
       options: filterOptions,
       data: responses,
     })
 
+  // Function to calculate the average score of responses
   const calcAverageScore = (responses: Response[]) => {
     const total = responses.reduce((sum, response) => {
       return sum + response.score
@@ -38,22 +46,33 @@ export default function ResponsePage() {
     return (total / responses.length).toFixed(2)
   }
 
+  // Fetch responses data from an API endpoint when the component mounts
   useEffect(() => {
+    // Using async/await to be able to handle the promise coming from fetch
     const getResponses = async () => {
+      // Using fetch and the url matchin folder (api/responses/route.ts)
+      // Using get since that is what this route handles
       const response = await fetch("/api/responses", {
         method: "get",
       })
+
+      // Using a built in method on the fetch-response that converts the "stream" to JSON (object-like data)
       const result = (await response.json()) as { data: Response[] }
+      // Using the state-setter to update the state after component is mounted
       setResponses(result.data)
     }
+
+    // Call the fetch function
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     getResponses()
   }, [])
 
+  // Function to handle response deletion
   const handleDelete = (id: string) => {
     setResponses((prev) => prev.filter((response) => response.id !== id))
   }
 
+  // What to render if no responses
   if (!responses.length) {
     return (
       <div className="mx-auto w-full max-w-3xl">
@@ -65,6 +84,8 @@ export default function ResponsePage() {
   return (
     <div className="mx-auto mt-8 w-full max-w-4xl">
       <section className="flex gap-4">
+        {/* Using the Filter component with necessary props */}
+        {/* Using .map to traverse the filterOptions-array */}
         {filterOptions.map((option) => (
           <Filter
             key={option.key}
@@ -73,15 +94,16 @@ export default function ResponsePage() {
             options={options[option.key]}
             label={option.label}
             onChange={(e) => {
-              handleFilter(e, option.key)
+              handleFilter(e, option.key) // Using the handleFilter that comes from the custom hook
             }}
             onSubmit={(e) => {
               e.preventDefault()
-              resetFilter(option.key)
+              resetFilter(option.key) // Using the resetFilter that comes from the custom hook
             }}
           />
         ))}
       </section>
+      {/* Using "children" in Responses to be able to have the flexibility to add whatever we like */}
       <Responses>
         <TableHead headers={[...Object.keys(responses[0]), "Actions"]} />
         <tbody>
@@ -95,6 +117,7 @@ export default function ResponsePage() {
               </td>
             </tr>
           ) : null}
+          {/* Using ResponseItem component and provides necessary props to this component */}
           {data.map((response) => (
             <ResponseItem
               key={response.id}
@@ -102,6 +125,7 @@ export default function ResponsePage() {
               {...response}
             />
           ))}
+          {/* Using a final ResponsItem component but only shows the score which is a calculated value */}
           {data.length ? (
             <ResponseItem
               id={""}
--- webapp-03/src/components/Filter.tsx
+++ webapp-03/src/components/Filter.tsx
@@ -1,7 +1,9 @@
+// Importing React and relevant types.
 import "react"
 
 import type { ChangeEventHandler, FormEventHandler } from "react"
 
+// Defining a functional component 'Filter'.
 export function Filter(props: {
   filterValue: string
   onChange: ChangeEventHandler<HTMLSelectElement>
@@ -10,14 +12,18 @@ export function Filter(props: {
   options: string[]
   onSubmit: FormEventHandler<HTMLFormElement>
 }) {
+  // Destructuring props for ease of use.
   const { id, label, options, filterValue, onChange, onSubmit } = props
 
+  // Rendering a form component with label, select input, and optional reset button.
   return (
-    <form onSubmit={onSubmit} className="flex flex-col gap-1 ">
+    <form onSubmit={onSubmit} className="flex flex-col gap-1">
+      {/* Label for the select input */}
       <label className="font-bold" htmlFor={id}>
         {label}
       </label>
       <div className="flex gap-1">
+        {/* Select input for filtering */}
         <select
           className="max-w-fit"
           id={id}
@@ -27,12 +33,14 @@ export function Filter(props: {
           <option value="" disabled>
             Choose filter
           </option>
+          {/* Mapping over options to generate select input options */}
           {options.map((option) => (
             <option key={option} value={option}>
               {option}
             </option>
           ))}
         </select>
+        {/* Conditional rendering of reset button */}
         {filterValue ? (
           <button className="flex items-center justify-center border border-red-300 px-4 text-red-600">
             X
